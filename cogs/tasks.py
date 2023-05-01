0c39d01f7abdfc10501262486fc4447ef27991f8
Paul Philion <pphlilion@istreamplanet.com>
2023-05-01 00:52:02 -0700

    porting more tests

--- cogs/tasks.py
+++ cogs/tasks.py
@@ -12,6 +12,7 @@ async def setup(bot):
     manager = TaskManager(store)
     await bot.add_cog(TaskCog(bot, manager))
 
+
 # maps arg strs into param dicts.
 class ParamMapper(commands.Converter):
     def parse(self, param_str):
@@ -140,15 +141,121 @@ class TaskManager():
         return id
     
     def edit(self, id, params):
-        self.bot.store.update(id, params)
+        self.store.update(id, params)
         pass
+
+    def get(self, id):
+        return self.store.row(id)
 
     def list(self, params):
         log.debug(f"### list {params}")
-        return self.bot.store.find(params)
+        return self.store.find(params)
+    
+    def fieldnames(self):
+        return self.store.fieldnames
 
 
-   
+# gspread
+import gspread
+class SheetsBackingStore:
+    def __init__(self, name: str):
+        gc = gspread.service_account()
+        sh = gc.open(name)
+        self.sheet = sh.sheet1
+        self.fieldnames = self.sheet.row_values(1)
+        self.field_map = {}
+        # index of fields, and their 1-based sheet index
+        for i, field in enumerate(self.fieldnames):
+            self.field_map[field] = i + 1
+
+    def count(self):
+        # just get the number of values in id col
+        col = self.field_map['id']
+        return len(self.sheet.col_values(col))
+
+        
+    # get all the values as dict[]
+    def values(self):
+        return self.sheet.get_all_records()
+
+    def find_id(self, id):
+        col = self.field_map['id']
+        log.debug(f"looking for {id} in col={col}")
+        cell = self.sheet.find(str(id), in_column=col) # rampant str() - find() failed for int(id)
+        if cell:
+            log.debug(f"found row: {cell.row}")
+            return cell
+        else:
+            log.debug(f"Couldn't: id={id} in col={col}")
+
+    def row(self, id):
+        cell = self.find_id(id)
+        if cell:
+            values = self.sheet.row_values(cell.row)
+            # map columns items to names
+            row = {}            
+            #log.debug(f'{values}')
+
+            for col, value in enumerate(values):
+                name = self.fieldnames[col]
+                #log.debug(f'{name}')
+                row[name] = value
+
+            return row
+
+    def get(self, id, field):
+        found = self.row(id)
+        if found:
+            return found[field]
+
+    # return any fields that match all suppied params (they are "and"ed)
+    def find(self, fields):
+        result = []
+
+        # more dumb search
+        for row in self.values():
+            for key, value in fields.items():
+                if row[key] != value:
+                    break;
+            else: # all values match!
+                result.append(row)
+
+        return result
+
+    def update(self, id, params):
+        cell = self.find_id(id)
+
+        # update the values
+        for name, value in params.items():
+            col = self.field_map[name]
+            #log.debug(f"Updating a value: {id}, {name}/{col} -> {value}")
+
+            self.sheet.update_cell(cell.row, col, value)
+
+
+    def add(self, params):
+        log.debug(f"add:params={params}")
+
+        # create row value list
+        row = [None] * (len(self.field_map.items()) + 1)
+
+        for field, value in params.items():
+            col = self.field_map.get(field)
+            if col:
+                row[col] = value
+                #log.debug(f"add: {col}/{field} = {params}")
+            else:
+                log.warn(f'Unknown column name: {field}')
+
+        # the list is too long currently, due to the 1-based index for sheets
+        row = row[1:]
+
+        # create a new row
+        log.debug(f"Appending row, id={row[0]}: {row}")
+        self.sheet.append_row(row)
+
+        return params['id']
+
     
 class FileBackingStore:
     def __init__(self, filename: str):
--- test_general.py
+++ test_general.py
@@ -5,7 +5,7 @@ import pytest
 import pytest_asyncio
 import discord.ext.test as dpytest
 
-from cogs.tasks import ParamMapper, FileBackingStore
+from cogs.tasks import ParamMapper, FileBackingStore, SheetsBackingStore, TaskManager
 
 @pytest.mark.asyncio
 async def test_ping(bot):
@@ -52,21 +52,21 @@ def test_params():
         assert result == expected[i]
 
 
-def test_fieldnames():
+def test_file_fieldnames():
     store = FileBackingStore("test.csv")
     assert store.fieldnames == ['id', 'title', 'project', 'status', 'assigned', 'updated', 'notes']
 
-def test_value():
+def test_file_value():
     store = FileBackingStore("test.csv")
     assert store.get('4', 'project') == 'yoyo'
     assert store.get('1', 'assigned') == 'philion'
 
-def test_find():
+def test_file_find():
     store = FileBackingStore("test.csv")
 
     assert store.find({'status': 'jolly'})[0]['id'] == "4"
 
-def test_update():
+def test_file_update():
     store = FileBackingStore("test.csv")
 
     # confirm status=jolly
@@ -79,3 +79,58 @@ def test_update():
     # reset to orig
     store.update('4', {'status': 'jolly'})
     assert store.get('4', 'status') == 'jolly'
+
+# google sheets
+import random
+import datetime
+def get_sheets_store():
+    try:
+        store = SheetsBackingStore("Taskbot Test Sheet")
+        return TaskManager(store) # id handling was moved to task manager
+    except Exception as ex:
+        pytest.skip(f"Cannot load config: {ex}")
+
+def test__sheet_add():
+    tasks = get_sheets_store()
+    row = {
+        'title': f'Test title {random.randint(42, 9999)}',
+        'project': random.choice(['acme','rocket','yoyo','skateboard']),
+        'status': random.choice(['new','open','working','rejected','closed']),
+        'assigned': random.choice(['unassigned','philion','wile','bugs','roadrunner']),
+        'updated': datetime.datetime.now().isoformat(sep=' ', timespec='seconds')
+    }
+
+    id = tasks.add(row)
+    assert id is not None, "id is missing from add() response"
+
+    testrow = tasks.get(id)
+
+    for name, value in row.items():
+        assert str(testrow[name]) == str(value)
+
+def test_fieldnames():
+    tasks = get_sheets_store()
+    assert tasks.fieldnames() == ['id', 'title', 'project', 'status', 'assigned', 'updated', 'notes']
+
+def test_value():
+    tasks = get_sheets_store()
+    assert tasks.get('4')['project'] == 'yoyo'
+    assert tasks.get('1')['assigned'] == 'philion'
+
+def test_sheets_list():
+    tasks = get_sheets_store()
+    assert tasks.list({'status': 'jolly'})[0]['id'] == 4
+
+def test_sheets_edit():
+    tasks = get_sheets_store()
+
+    # confirm status=jolly
+    assert tasks.get('4')['status'] == 'jolly'
+
+    # update
+    tasks.edit('4', {'status': 'perturbed'})
+    assert tasks.get('4')['status'] == 'perturbed'
+
+    # reset to orig
+    tasks.edit('4', {'status': 'jolly'})
+    assert tasks.get('4')['status'] == 'jolly'
